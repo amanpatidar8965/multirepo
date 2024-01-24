e024d662c407a5c849447f76533658be11f728c2
Leetcode <amanpatidar0681@gmail.com>
2024-01-24 13:36:39 +0530

    Third branch code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { LibModule } from './lib/lib.module';
+import { LabModule } from './lab/lab.module';
 
 
 @Module({
-  imports: [UserModule, AuthModule, LibModule],
+  imports: [UserModule, AuthModule, LibModule, LabModule],
   controllers: [AppController],
   providers: [],
 })
